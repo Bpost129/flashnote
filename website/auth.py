f1710bfa5662bae97e0736b5fc4210decae29dd9
Bpost129 <Bpost129@yahoo.com>
2025-01-22 16:59:30 -0500

    add title field to note model, add methods for creating and deleting notes, render note form and created notes

--- website/auth.py
+++ website/auth.py
@@ -2,6 +2,7 @@ from flask import Blueprint, render_template, request, redirect, url_for
 from werkzeug.security import generate_password_hash, check_password_hash
 from .models import User
 from . import db
+from flask_login import login_user, logout_user, login_required, current_user
 
 auth = Blueprint('auth', __name__)
 
@@ -15,6 +16,7 @@ def login():
     if user:
       if check_password_hash(user.password, password):
         print('logged in')
+        login_user(user, remember=True)
         return redirect(url_for('views.home'))
       else:
         print('incorrect password')
@@ -22,11 +24,13 @@ def login():
       print('user doesnt exist')
 
 
-  return render_template('login.html')
+  return render_template('login.html', user=current_user)
 
 @auth.route('/logout')
+@login_required
 def logout():
-  return "<p>Logout</p>"
+  logout_user()
+  return redirect(url_for('auth.login'))
 
 @auth.route('/sign-up', methods=['GET', 'POST'])
 def sign_up():
@@ -44,8 +48,9 @@ def sign_up():
       new_user = User(email=email, first_name=first_name, password=generate_password_hash(password))
       db.session.add(new_user)
       db.session.commit()
+      login_user(user, remember=True)
       print('user created!')
       return redirect(url_for('views.home'))
 
 
-  return render_template('sign_up.html')
+  return render_template('sign_up.html', user=current_user)
--- website/views.py
+++ website/views.py
@@ -1,7 +1,41 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request, jsonify
+from flask_login import login_required, current_user
+from .models import Note
+from . import db
+import json
 
 views = Blueprint('views', __name__)
 
 @views.route('/')
+@login_required
 def home():
-  return render_template("home.html")
+  return render_template("home.html", user=current_user)
+
+@views.route('/notes', methods=['GET', 'POST'])
+@login_required
+def notes():
+  if request.method == 'POST':
+    title = request.form.get('title')
+    content = request.form.get('content')
+
+    if len(content) < 1:
+      print('note is too short')
+    else:
+      new_note = Note(title=title, content=content, user_id=current_user.id)
+      db.session.add(new_note)
+      db.session.commit()
+      print('note added')
+
+  return render_template("notes.html", user=current_user)
+
+@views.route('/delete-note', methods=['POST'])
+def delete_note():
+  note = json.loads(request.data)
+  noteId = note['noteId']
+  note = Note.query.get(noteId)
+  if note:
+    if note.user_id == current_user.id:
+      db.session.delete(note)
+      db.session.commit()
+
+  return jsonify({})
