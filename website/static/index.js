a442d0fdfb4fdc8d259d2e53fc1cb507878e1878
Bpost129 <Bpost129@yahoo.com>
2025-01-30 22:33:02 -0500

    enable light/dark mode switching

--- website/static/index.js
+++ website/static/index.js
@@ -7,3 +7,27 @@ function deleteNote(noteId) {
     window.location.href = "/notes"
   })
 }
+
+// theme change
+
+const lightDarkBtn = document.getElementById('theme')
+const body = document.body
+
+const theme = localStorage.getItem('theme')
+
+if (theme) {
+  body.classList.add(theme)
+}
+
+
+lightDarkBtn.onclick = () => {
+  if (body.classList.contains('light')) {
+    body.classList.remove('light')
+    body.classList.add('dark')
+    localStorage.setItem('theme', 'dark')
+  } else {
+    body.classList.remove('dark')
+    body.classList.add('light')
+    localStorage.setItem('theme', 'light')
+  }
+}
